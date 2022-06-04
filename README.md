92e12d3660d43d01abeda5ac32c307f00de37b71
Norivan Oliveira Silva <norivan.oliveira@gmail.com>
2022-06-03 21:56:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Projeto Base Spring Boot
---
+++ src/main/java/br/com/itau/app/BoletoApp.java
@@ -0,0 +1,14 @@
+package br.com.itau.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class BoletoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoletoApp.class, args);
+    }
+
+}
